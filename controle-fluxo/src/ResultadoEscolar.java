29fa40e89608f61249b00f96629370329f448f85
gabriela.marcolan <gabimarcolan@hotmail.com>
2024-05-26 20:08:34 -0300

    Condicional encadeada

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,8 +1,10 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 8;
+        int nota = 6;
             if(nota >=7)
                 System.out.println("Aprovado");
+            else if(nota >= 5 && nota <7)
+                System.out.println("Recuperação");
             else
                 System.out.println("Reprovado");
     }
