0199033c80c1ed2e10e24403d9eb9849f50e601d
gabriela.marcolan <gabimarcolan@hotmail.com>
2024-06-03 21:56:55 -0300

    Finalizar

--- controle-fluxo/src/ProcessoSeletivo.java
+++ controle-fluxo/src/ProcessoSeletivo.java
@@ -1,8 +1,8 @@
 public class ProcessoSeletivo {
 public static void main(String[] args) {
     analisarCandidato(1900.0);
-    analisarCandidato(2000.0);
     analisarCandidato(2200.0);
+    analisarCandidato(2000.0);
 }
 static void analisarCandidato (double salarioPretendido) {
     double salarioBase = 2000.0;
