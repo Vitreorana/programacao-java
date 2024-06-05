f91f085567fbb20a352d5d266a694cfe70b769b0
gabriela.marcolan <gabimarcolan@hotmail.com>
2024-06-04 22:47:34 -0300

    Atualização

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -2,30 +2,26 @@ import java.util.Scanner;
 
 public class Contador {
     public static void main(String[] args) throws ParametrosInvalidosException {
-        try{
         Scanner scanner = new Scanner(System.in);
 		System.out.println("Digite o primeiro parâmetro");
-
 		int parametroUm = scanner.nextInt();
 		System.out.println("Digite o segundo parâmetro");
-
 		int parametroDois = scanner.nextInt();
-		
-		contar(parametroUm, parametroDois);
         scanner.close();
+        try{
+        contar(parametroUm, parametroDois);
         }
-     
-    catch(Exception e) {
-        System.out.println("O segundo parâmetro deve ser maior que o primeiro.");
+        catch(Exception e) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro.");
     }
 }
 	static void contar (int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
 		if (parametroUm >= parametroDois){
             throw new ParametrosInvalidosException ("O segundo parâmetro deve ser maior que o primeiro.");
-        }
-        else {
-            for (int parametro = 1; parametro <= 100; parametro ++) {
-	            System.out.println("Contando...");
+        }else {
+            for (int parametro = 1 ; parametro <= parametroDois ; parametro ++) {
+	            int numero = parametroDois - parametroUm;
+                System.out.println("Imprimindo o número  " + numero++);
             }
     }
     }
