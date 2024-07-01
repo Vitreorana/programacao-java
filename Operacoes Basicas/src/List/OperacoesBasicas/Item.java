9dc0a152e32c1cfb9a8aceccb6a5d45b4084a85c
gabriela.marcolan <gabimarcolan@hotmail.com>
2024-06-30 22:35:17 -0300

    Finalizei sem descobrir o erro

---
+++ Operacoes Basicas/src/List/OperacoesBasicas/Item.java
@@ -0,0 +1,29 @@
+package List.OperacoesBasicas;
+
+public class Item {
+    private String nome;
+    private double preco;
+    private int quant;
+
+    public Item(String nome, double preco, int quant){
+        this.nome = nome;
+        this.preco = preco;
+        this.quant = quant;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public double getPreco(){
+        return preco;
+    }
+    public int getQuant(){
+        return quant;
+    }
+    public String toString(){
+        return "Item{" +
+            "nome='" + nome + '/' +
+            ",preco=" + preco +
+            ", quant=" + quant +
+            '}';
+    }
+}
