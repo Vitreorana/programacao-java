bfddc8a165c3bac168c2f0b5b45bb790cd1f8fec
gabriela.marcolan <gabimarcolan@hotmail.com>
2024-07-04 09:52:21 -0300

    Pesquisa em List

--- Operacoes Basicas/src/List/PesquisaEmList/Livro.java
+++ Operacoes Basicas/src/List/PesquisaEmList/Livro.java
@@ -4,7 +4,7 @@ public class Livro {
     private String titulo;
     private String autor;
     private int anoPublicacao;
-public Livro(String titulo, String autor, int anoPublicacao){
+public Livro(String titulo, String autor, Class<Integer> class1){
     this.titulo = titulo;
     this.autor = autor;
     this.anoPublicacao = anoPublicacao;
