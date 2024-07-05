d38ad52246160f0855d6216709b8a008ddde0ce9
gabriela.marcolan <gabimarcolan@hotmail.com>
2024-07-04 21:22:59 -0300

    Não descobri o erro.

---
+++ Operacoes Basicas/src/Set/OperacoesBasicas/ConjuntoConvidados.java
@@ -0,0 +1,74 @@
+package Set.OperacoesBasicas;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoConvidados{
+    //atributo:
+    private Set<Convidado> convidadosSet;
+
+    public ConjuntoConvidados(){
+        this.convidadosSet = new HashSet<>();
+    }
+    //métodos:
+    public void adicionarConvidadoPorCodigoConvite(int codigoConvite){
+        convidadosSet.add(new Convidado(nome, codigoConvite));
+    }
+    public void removerConvidadoPorCodigoConvite(int codigoConvite){
+        Convidado convidadoParaRemover = null;
+        // se o convidado cujo código de convite for igual ao codigo c, 
+        // nós vamos remover o convidado:
+        if(!convidadosSet.isEmpty()){
+            for (Convidado c: convidadosSet){
+                if(c.getCodigoConvite() == codigoConvite){
+                convidadoParaRemover = c;
+                // feito isso eu paro esse laço de repetição, 
+                // pq se eu encontrei um elemento dentro de um set, não vai ter mais um igual a esse,
+                //então eu paro:
+                break; 
+            }
+        }
+        convidadosSet.remove(convidadoParaRemover);
+    }else{
+        throw new RuntimeException("O conjunto está vazio!");
+    }
+    }
+    public int contarConvidados(){
+        return convidadosSet.size();
+    }
+    public void exibirConvidado(){
+        if (!convidadosSet.isEmpty()){
+            System.out.println(convidadosSet);
+        }else{
+            System.out.println("O conjunto está vazio!");
+        }
+        }
+    public static void main(String[] args){
+        //criando uma instância da classe ConjuntoConvidados:
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+
+        //exibindo o número de convidados no conjunto (deve ser zero):
+        System.out.println("Existem" + conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de Convidados");
+ 
+        // adicionando convidados ao conjunto:
+        conjuntoConvidados.adicionarConvidado("Alice", 1234);
+        conjuntoConvidados.adicionarConvidado("Bob", 1235);
+        conjuntoConvidados.adicionarConvidado("Charlie", 1235);
+        conjuntoConvidados.adicionarConvidado("David", 1236);
+
+        //exibindo os convidados no conjunto:
+        System.out.println("Convidaados no conjunto:");
+        conjuntoConvidados.exibirConvidado();
+
+        //exibindo o número atualizado de convidados no conjunto
+        System.out.println("Existem"+ conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de convidados");
+
+        //removendo um convidado do conjunto por código de convite:
+        conjuntoConvidados.removerConvidadoPorCodigoConvite(1236);
+        System.out.println("existem" + conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de Convidados após a remoção");
+
+        //exibindo os convidados atualizados no conjunto:
+        System.out.println("Convidados no conjunto após a remoção:");
+        conjuntoConvidados.exibirConvidado();
+    }
+    }
---
+++ Operacoes Basicas/src/Set/OperacoesBasicas/Convidado.java
@@ -0,0 +1,33 @@
+package Set.OperacoesBasicas;
+
+public class Convidado {
+    //atributos:
+    private String nome;
+    private Integer codigoConvite;
+    //constructor:
+public Convidado(String nome, int codigoConvite){
+    this.nome = nome;
+    this.codigoConvite = codigoConvite;
+}
+public String getNome(){
+    return nome;
+}
+public int getCodigoConvite(){
+    return codigoConvite;
+}
+public boolean equals(Object o){
+    if(this ==o) return true;
+    if(!(o instanceof Convidado convidado)) return false;
+    return getCodigoConvite() == convidado.getCodigoConvite();
+}
+// para dizer qual elemento vc quer que seja comparado para dizer que um elemento é igual ao outro ou não:
+public int hashCode(){
+    return Objects.hash(getCodigoConvite());
+}
+public String toString(){
+    return "Convidado{" +
+    "nome=" + nome + '\'' +
+    ", codigoConvite=" + codigoConvite +
+    '}';
+}
+}
