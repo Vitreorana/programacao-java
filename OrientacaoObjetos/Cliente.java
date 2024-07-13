61875ef9b5c4e39e7545d69f10c2b6d63a8f007a
gabriela.marcolan <gabimarcolan@hotmail.com>
2024-07-13 16:34:47 -0300

    Exemplo

---
+++ OrientacaoObjetos/Cliente.java
@@ -0,0 +1,9 @@
+public class Cliente {
+    private String nome;
+    public String getNome(){
+        reurn nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+}
