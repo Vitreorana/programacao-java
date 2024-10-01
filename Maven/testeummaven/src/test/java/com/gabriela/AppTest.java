84b072b0b70126224f721bd0559ed6be516cbeed
gabriela.marcolan <gabimarcolan@hotmail.com>
2024-10-01 10:38:10 -0300

    Criando Projeto

--- Maven/testeummaven/src/test/java/com/gabriela/AppTest.java
+++ Maven/testeummaven/src/test/java/com/gabriela/AppTest.java
@@ -8,13 +8,17 @@ import org.junit.Test;
  * Unit test for simple App.
  */
 public class AppTest 
-{
-    /**
+{    /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
+    public void shouldAnswerWithTrue(){
         assertTrue( true );
+    }
+    @Test
+    public void testSoma(){
+        int num1 = 1;
+        int num2 = 2;
+        assertEquals(2, num1 + num2);
     }
 }
